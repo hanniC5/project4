6949d25892a0757b2a4d1eff1c4f1cc3f244bab4
Hannah Crump <hcrump5@gmail.com>
2018-05-10 14:12:14 -0400

    js for inputs working

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,14 @@ const emotionApp = {};
 
 emotionApp.emotions = {};
 
-const userImage = 'http://www.rmmagazine.com/wp-content/uploads/2012/08/Sad-Face.png';
+emotionApp.attributes = {};
+
+emotionApp.userImage = '';
+emotionApp.userName = '';
 
 // in this case, whatever userImage is will be passed as picture to processImage function
 
 emotionApp.processImage = (picture) => {
-	// Request parameters.
 	const params = {
 		"returnFaceId": "false",
 		"returnFaceLandmarks": "false",
@@ -26,22 +28,46 @@ emotionApp.processImage = (picture) => {
 		data: `{"url": "${picture}"}`,
 	})
 	.then( function(response){
-		console.log(response);
+		// console.log(response);
 		emotionApp.getEmotions(response);
+		emotionApp.getAttributes(response);
+		console.log(emotionApp.emotions);
 	});
 } 
-
+//function getting the data into an emotion object store in emotionApp.emotions 
 emotionApp.getEmotions = function(res) {
 	const face = res[0];
 	const emotions = face.faceAttributes.emotion;
-	// console.log(emotions);
 	emotionApp.emotions = Object.assign(emotions);
 };
-
+//function getting attribute data and putting into attribute object 
+emotionApp.getAttributes = function(res) {
+	const face= res[0];
+	const {age, smile, glasses} = face.faceAttributes;
+	const {yaw} = face.faceAttributes.headPose;
+	emotionApp.attributes.age = age;
+	emotionApp.attributes.smile = smile;
+	emotionApp.attributes.yaw = yaw;
+	if (glasses === "NoGlasses") {
+		emotionApp.attributes.glasses = false;
+	} else {
+		emotionApp.attributes.glasses = true;
+	}
+	console.log(emotionApp.attributes);
+};
+//function that take the user inputs and gets them into global variables to be used
+emotionApp.userInputs = function() {
+	$('form').on('submit', () => {
+		emotionApp.userName = $('input[name=name]').val();
+		console.log(emotionApp.userName);
+		emotionApp.userImage = $('input[name=imagePath]').val();
+		this.processImage(emotionApp.userImage);
+	});
+};
 
 // INIT FUNCTION
 emotionApp.init = function(){
-	emotionApp.processImage(userImage);
+	emotionApp.userInputs();
 }
 
 // DOCUMENT READY
