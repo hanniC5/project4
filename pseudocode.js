b6ae1016c5aba672348c430fd6dae01032c60585
Hannah Crump <hcrump5@gmail.com>
2018-05-10 12:31:52 -0400

    initial HTML and gitignore

--- pseudocode.js
+++ pseudocode.js
@@ -8,3 +8,10 @@
 // for example, high anger level would make red background
 // sadness, cursive text
 
+
+//AGE 'Your age is'
+//GLASSES - "Glasses", "NoGlasses", "ReadingGlasses"
+//Smile - rating from 0-1
+
+
+//SAD FACE: 69%: http://www.rmmagazine.com/wp-content/uploads/2012/08/Sad-Face.png
