0c1ea1a71811b19b0a6011f7a31cdbf28b2a8a26
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-05-12 15:58:46 -0400

    news and song populating

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,7 +1,6 @@
 const playlists = {
-	anger: `<iframe src="http://8tracks.com/mixes/908129/player_v3_universal" width="100%" height="100%" style="border: 0px none;"></iframe>
-		<p class="_8t_embed_p" style="font-size: 11px; line-height: 12px;"><a href="http://8tracks.com/oohdreii90/punch-in-the-face?utm_medium=referral&utm_content=mix-page&utm_campaign=embed_button">Punch in the Face!</a> from <a href="http://8tracks.com/oohdreii90?utm_medium=referral&utm_content=mix-page&utm_campaign=embed_button">oohdreii90</a> on <a href="http://8tracks.com?utm_medium=referral&utm_content=mix-page&utm_campaign=embed_button">8tracks Radio</a>.</p>`,
-		
+	anger : `<iframe src="http://8tracks.com/mixes/908129/player_v3_universal" width="100%" height="100%" style="border: 0px none;"></iframe>`,
+
 	sadness: 'https://8tracks.com/book-em-spacemonkey/almost-blue',
 	happiness: 'https://8tracks.com/kateandlate/be-happy',
 	contempt: 'https://8tracks.com/britania/if-i-had-a-heart',
@@ -10,11 +9,12 @@ const playlists = {
 	neutral: 'https://8tracks.com/yokv/chill-party-vibes-2018#smart_id#play',
 }
 
-const newsItem = {
+const newsItems = {
 	anger: {
-		headline: '',
-		url : '',
-		photo : '',
+		headline: 'Airport Erupts in Cheers When Lucky Flight Delay Leads to Soldier Getting to Watch Birth of His Baby',
+		url: 'https://www.goodnewsnetwork.org/lucky-flight-delay-leads-to-soldier-getting-to-watch-birth-of-his-daughter-as-airport-cheers/',
+		photo : '????',
+		source: `source: Good News Network`
 	},
 	sadness: '?',
 	happiness: '?',
